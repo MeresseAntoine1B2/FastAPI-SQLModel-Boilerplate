f2727bd0882992696e733eccd14603362ab74f06
MeresseAntoine1B2 <antoine@meresse.info>
2025-02-19 15:12:41 +0100

    :tada: Initial commit

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.api.v1.endpoints import auth, users
+from app.db.session import engine
+from sqlmodel import SQLModel
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+
+app = FastAPI(title="FastAPI Boilerplate", lifespan=lifespan)
+
+app.include_router(auth.router, prefix="/api/v1", tags=["auth"])
+app.include_router(users.router, prefix="/api/v1", tags=["users"])
